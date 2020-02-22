4d54cda77558dcbdb9dff55098b72be7e09cedba
Megnath Ramesh <megnath@ualberta.ca>
2020-02-22 15:26:07 -0700

    Clean up shell script

--- run_jplHackathon_docker.sh
+++ run_jplHackathon_docker.sh
@@ -1,1 +1,9 @@
-sudo docker run  -p 5900:5900 -p 9000:9000 -p 6080:80 -p 5601:5601 -p 8888:8888 -v /home/yourusername/dockervolumes/awsjpl:/data -v /home/yourusername/GitHub/AWS-JPL-OSR-Challenge:/home/ubuntu/catkin_ws/src -v /home/yourusername/dockervolumes/awsjpl-gazebo:/root/.gazebo --gpus all \ awsjpl
+sudo docker run -p 5900:5900 \
+                -p 9000:9000 \
+                -p 6080:80 \
+                -p 5601:5601 \
+                -p 8888:8888 \
+                -v /home/megnath/dockervolumes/awsjpl:/data \
+                -v /home/megnath/git/docker-setup/AWS-JPL-OSR-Challenge:/home/ubuntu/catkin_ws/src \
+                -v /home/megnath/dockervolumes/awsjpl-gazebo:/root/.gazebo \
+                awsjpl
